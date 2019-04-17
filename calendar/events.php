ca6c5375ad47cac7738581a7dc374b332c4e4bb1
bryan <yiyuan@ucsd.edu>
2019-04-16 17:19:30 -0700

    fixed all navbars and navlinks

--- calendar/events.php
+++ calendar/events.php
@@ -40,6 +40,8 @@ $events = $results->getItems();
 <!DOCTYPE html>
 <html>
     <head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>SSA: Calendar</title>
 		<link rel="stylesheet"
 			href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
