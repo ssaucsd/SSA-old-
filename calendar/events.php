71430a5d30d1af2feb9d1e55d5f0f5f2ecbf124d
Bryan Yuan <43862318+bryanyuan1@users.noreply.github.com>
2019-05-21 21:49:47 -0700

    deleted hard-coded event from calendar page (#30)
    * removed hard-coded event and adjusted space
    
    * fixed link to about page in calendar page

--- calendar/events.php
+++ calendar/events.php
@@ -77,7 +77,7 @@ $events = $results->getItems();
 											</a>
 									</li>
 									<li class="nav-item flex-fill">
-											<a class="nav-link" id="links" href="#">
+											<a class="nav-link" id="links" href="../about">
 											About
 											</a>
 									</li>
@@ -119,14 +119,6 @@ $events = $results->getItems();
 					<div class="title-events">
 						<div class="title">EVENTS |</div>
 						<div class="events">
-							<div style="font-size: 60px; text-align: center; font-weight: 600" id="month-num">
-								09
-								<p id="month">SEP</p>
-							</div>
-							<div style="padding-top: 25px; text-align: right" class="event-text">
-								<div>Annual Fall Concert</div>
-								<div>Music Center 8:30pm</div>
-							</div>
 							<?php
 							if (empty($events)) {
 								echo '<p style="text-align: center;">No upcoming events.</p>';
